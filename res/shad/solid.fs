6fcdb34a9a9abf4f09712a96198d7f8af00d837b
JackHasaKeyboard <jackhasakeyboard@gmail.com>
2023-08-16 23:36:30 -0600

    Stripped arbitrary calculations in solid shader

--- res/shad/solid.fs
+++ res/shad/solid.fs
@@ -2,14 +2,8 @@
 
 in vec3 _pos;
 
-vec3 sun = vec3(1.0, 0.0, -1.0);
-
-const vec3 col = vec3(50, 50, 50);
+const vec3 col = vec3(50 / 255.0, 50 / 255.0, 50 / 255.0);
 
 void main() {
-	vec3 normFace = normalize(cross(dFdx(_pos), dFdy(_pos)));
-
-	float diff = max(dot(normFace, normalize(sun)), 0.0) * 0.2;
-
-	gl_FragColor = vec4((1.0 - diff) * (col / 255.0), 1.0);
+	gl_FragColor = vec4(col, 1.0);
 }
