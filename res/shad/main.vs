5a1b0a6c122409e54a48bbdadc2522cae40c0086
JackHasaKeyboard <jackhasakeyboard@gmail.com>
2023-08-16 23:07:59 -0600

    Implemented directional lighting

--- res/shad/main.vs
+++ res/shad/main.vs
@@ -6,6 +6,10 @@ uniform mat4 proj;
 
 in vec3 pos;
 
+out vec3 _pos;
+
 void main() {
 	gl_Position = proj * view * model * vec4(pos, 1.0);
+
+	_pos = pos;
 }
--- res/shad/solid.fs
+++ res/shad/solid.fs
@@ -1,5 +1,15 @@
 #version 400
 
+in vec3 _pos;
+
+vec3 sun = vec3(1.0, 0.0, -1.0);
+
+const vec3 col = vec3(255, 100, 255);
+
 void main() {
-	gl_FragColor = vec4(vec3(1.0, 0.0, 1.0), 1.0);
+	vec3 normFace = normalize(cross(dFdx(_pos), dFdy(_pos)));
+
+	float diff = max(dot(normFace, normalize(sun)), 0.0) * 0.2;
+
+	gl_FragColor = vec4((1.0 - diff) * (col / 255.0), 1.0);
 }
