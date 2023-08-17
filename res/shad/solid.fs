094b8a095975f0bcf4aeebe0ed333f8fed6e0675
JackHasaKeyboard <jackhasakeyboard@gmail.com>
2023-08-16 23:37:16 -0600

    Brought down brightness

--- res/shad/solid.fs
+++ res/shad/solid.fs
@@ -2,7 +2,7 @@
 
 in vec3 _pos;
 
-const vec3 col = vec3(50 / 255.0, 50 / 255.0, 50 / 255.0);
+const vec3 col = vec3(30 / 255.0, 30 / 255.0, 30 / 255.0);
 
 void main() {
 	gl_FragColor = vec4(col, 1.0);
