ab24269bf067fc139ded96aaa2c38486175e54e7
JackHasaKeyboard <jackhasakeyboard@gmail.com>
2023-08-16 23:55:48 -0600

    Added checkerboard shader

--- res/shad/solid.fs
+++ res/shad/solid.fs
@@ -2,8 +2,16 @@
 
 in vec3 _pos;
 
-const vec3 col = vec3(30 / 255.0, 30 / 255.0, 30 / 255.0);
+const float pad = 0.16;
+const float sz = 2.0;
+
+const float stride = sz + (pad * 2);
+
+vec3[2] grey = vec3[2](
+	vec3(30, 30, 30),
+	vec3(100, 100, 100)
+);
 
 void main() {
-	gl_FragColor = vec4(col, 1.0);
+	gl_FragColor = vec4(grey[int(mod(_pos.x, stride * 2) > stride ^^ mod(_pos.z, stride * 2) > stride)] / 225.0, 1.0);
 }
