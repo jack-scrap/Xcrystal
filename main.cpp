88b3d0826c6b9f145ee97d41c0d697ceff8701a8
JackHasaKeyboard <jackhasakeyboard@gmail.com>
2023-08-16 22:54:44 -0600

    Created 3D projection, continually rotated model

--- main.cpp
+++ main.cpp
@@ -25,6 +25,11 @@ int main() {
 	};
 	glBufferData(GL_ARRAY_BUFFER, sizeof vtc, vtc, GL_STATIC_DRAW);
 
+	glm::mat4 model = glm::mat4(1.0);
+
+	glm::mat4 view = glm::lookAt(glm::vec3(5, 5, 5), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+	glm::mat4 proj = glm::perspective(glm::radians(45.0), 800.0 / 600.0, 0.1, 100.0);
+
 	Prog prog("main", "solid");
 
 	prog.use();
@@ -32,6 +37,14 @@ int main() {
 	GLint attrPos = glGetAttribLocation(prog._id, "pos");
 	glVertexAttribPointer(attrPos, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*) 0);
 	glEnableVertexAttribArray(attrPos);
+
+	GLint uniModel = glGetUniformLocation(prog._id, "model");
+	GLint uniView = glGetUniformLocation(prog._id, "view");
+	GLint uniProj = glGetUniformLocation(prog._id, "proj");
+
+	glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
+	glUniformMatrix4fv(uniView, 1, GL_FALSE, glm::value_ptr(view));
+	glUniformMatrix4fv(uniProj, 1, GL_FALSE, glm::value_ptr(proj));
 
 	prog.unUse();
 
@@ -47,6 +60,10 @@ int main() {
 
 		glBindVertexArray(vao);
 		prog.use();
+
+		model = glm::rotate(model, (GLfloat) 0.01, glm::vec3(0, 1, 0));
+
+		glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
 
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
--- res/shad/main.vs
+++ res/shad/main.vs
@@ -1,7 +1,11 @@
 #version 400
 
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 proj;
+
 in vec3 pos;
 
 void main() {
-	gl_Position = vec4(pos, 1.0);
+	gl_Position = proj * view * model * vec4(pos, 1.0);
 }
