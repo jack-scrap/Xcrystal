99c70d148abf8321cdbcdfa0ddcc551745349a18
JackHasaKeyboard <jackhasakeyboard@gmail.com>
2023-08-16 23:30:09 -0600

    Added ground

--- main.cpp
+++ main.cpp
@@ -55,7 +55,7 @@ int main() {
 
 	glm::mat4 model = glm::mat4(1.0);
 
-	glm::mat4 view = glm::lookAt(glm::vec3(5, 5, 5), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+	glm::mat4 view = glm::lookAt(glm::vec3(5, 7, 5), glm::vec3(0, 2, 0), glm::vec3(0, 1, 0));
 	glm::mat4 proj = glm::perspective(glm::radians(45.0), 240.0 / 180.0, 0.1, 100.0);
 
 	Prog prog("main", "solid");
@@ -76,6 +76,49 @@ int main() {
 
 	prog.unUse();
 
+	// Ground
+	GLuint vaoGround;
+	glGenVertexArrays(1, &vaoGround);
+	glBindVertexArray(vaoGround);
+
+	GLuint vboGround;
+	glGenBuffers(1, &vboGround);
+	glBindBuffer(GL_ARRAY_BUFFER, vboGround);
+
+	GLfloat vtcGround[] = {
+		-100.0, 0.0, -100.0,
+		100.0, 0.0, -100.0,
+		100.0, 0.0, 100.0,
+
+		100.0, 0.0, 100.0,
+		-100.0, 0.0, -100.0,
+		-100.0, 0.0, 100.0
+	};
+	glBufferData(GL_ARRAY_BUFFER, sizeof vtcGround, vtcGround, GL_STATIC_DRAW);
+
+	Prog progGround("main", "ground");
+
+	glm::mat4 modelGround = glm::mat4(1.0);
+
+	glm::mat4 viewGround = glm::lookAt(glm::vec3(5, 7, 5), glm::vec3(0, 2, 0), glm::vec3(0, 1, 0));
+	glm::mat4 projGround = glm::perspective(glm::radians(45.0), 240.0 / 180.0, 0.1, 100.0);
+
+	progGround.use();
+
+	GLint attrPosGround = glGetAttribLocation(prog._id, "pos");
+	glVertexAttribPointer(attrPosGround, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*) 0);
+	glEnableVertexAttribArray(attrPosGround);
+
+	GLint uniModelGround = glGetUniformLocation(prog._id, "model");
+	GLint uniViewGround = glGetUniformLocation(prog._id, "view");
+	GLint uniProjGround = glGetUniformLocation(prog._id, "proj");
+
+	glUniformMatrix4fv(uniModelGround, 1, GL_FALSE, glm::value_ptr(modelGround));
+	glUniformMatrix4fv(uniViewGround, 1, GL_FALSE, glm::value_ptr(viewGround));
+	glUniformMatrix4fv(uniProjGround, 1, GL_FALSE, glm::value_ptr(projGround));
+
+	progGround.unUse();
+
 	SDL_Event e;
 	unsigned int i = 0;
 	while (disp.open) {
@@ -90,12 +133,21 @@ int main() {
 		glBindVertexArray(vao);
 		prog.use();
 
-		model = glm::translate(glm::mat4(1.0), glm::vec3(0, sin(i / 50.0) / 5.0, 0));
+		model = glm::translate(glm::mat4(1.0), glm::vec3(0, 2.5 + (sin(i / 50.0) / 5.0), 0));
 		model = glm::rotate(model, (GLfloat) (i / 100.0), glm::vec3(0, 1, 0));
 
 		glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
 
 		glDrawArrays(GL_TRIANGLES, 0, 2 * 2 * 2 * 3);
+
+		prog.unUse();
+		glBindVertexArray(0);
+
+		// Ground
+		glBindVertexArray(vaoGround);
+		progGround.use();
+
+		glDrawArrays(GL_TRIANGLES, 0, 2 * 2 * 3);
 
 		prog.unUse();
 		glBindVertexArray(0);
---
+++ res/shad/ground.fs
@@ -0,0 +1,15 @@
+#version 400
+
+in vec3 _pos;
+
+vec3 sun = vec3(1.0, 0.0, -1.0);
+
+const vec3 col = vec3(100, 100, 100);
+
+void main() {
+	vec3 normFace = normalize(cross(dFdx(_pos), dFdy(_pos)));
+
+	float diff = max(dot(normFace, normalize(sun)), 0.0) * 0.2;
+
+	gl_FragColor = vec4((1.0 - diff) * (col / 255.0), 1.0);
+}
